46b811db9d2c97ec1cfd643e20f978720b156477
Mac Bross <43281019+LUMacKenn@users.noreply.github.com>
2022-09-13 10:53:36 -0400

    Replace instance of backbutton with actionbutton (#57)

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -2,31 +2,35 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Container, Skeleton, Typography } from "@mui/material";
 
-import { BackButton, PrimaryHeader, Questionnaire } from "../components";
+import { ActionButton, PrimaryHeader, Questionnaire } from "../components";
 import { useSurvey } from "../fetchDataHooks";
 
 export default function Survey() {
   const { surveyId } = useParams();
   const { survey, isError, isLoading } = useSurvey(surveyId);
-  
+
   if (isLoading) {
-    return (
-      <Skeleton variant="rectangular" animation="wave" height={280} />
-    );
+    return <Skeleton variant="rectangular" animation="wave" height={280} />;
   }
 
   if (isError) {
-    return (
-      <div>Woops something went wrong...</div>
-    );
+    return <div>Woops something went wrong...</div>;
   }
 
   return (
     <>
-      <PrimaryHeader sx={{ position: "relative", textAlign: "center", padding: "16px" }}>
-        <BackButton
+      <PrimaryHeader
+        sx={{ position: "relative", textAlign: "center", padding: "16px" }}
+      >
+        <ActionButton
+          type={"back"}
           route="/"
-          sx={{ alignSelf: "baseline", zIndex: 100, position: "absolute" , top: 0}}
+          sx={{
+            alignSelf: "baseline",
+            zIndex: 100,
+            position: "absolute",
+            top: 0,
+          }}
         />
 
         <Typography variant="h4" component="h1" sx={{ width: "70%" }}>
